c9894755d25bcb2584b6b9fe84688344548a63f3
freelancer-hah <alltimefree4793@gmail.com>
2026-07-17 18:33:16 +0500

    Email Configuration

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
-import recommendationRoutes from './routes/recommendationRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
